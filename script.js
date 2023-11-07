710499190c5e55af1820a6f16307199da006ad40
sanket sonawane <sanket28896@gmail.com>
2023-11-07 11:43:51 +0530

    description change

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var mnu = document.querySelector(".ri-menu-3-line");
 var clos = document.querySelector(".ri-close-line");
 var nav = document.querySelector(".resp-nav");
 
-mnu.addEventListener("click", function(){
-    nav.style.right = "0%";
-})
+mnu.addEventListener("click", function () {
+  nav.style.right = "0%";
+});
 
-clos.addEventListener("click", function(){
-    nav.style.right = "-50%";
-})
+clos.addEventListener("click", function () {
+  nav.style.right = "-50%";
+});
